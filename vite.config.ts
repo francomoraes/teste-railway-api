46bd6b1249e347efee3be6bfa6e52895a4a36400
Franco Moraes <francoapmoraes@gmail.com>
2024-10-27 13:40:41 -0300

    :wrench: chore: configurações iniciais de tailwind, prettier e docker

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    watch: {
+        usePolling: true,
+    },
+    host: true,
+    port: 5173
+  },
+          base: '',
+        resolve: {
+            alias: {
+                '@': '/src',
+            },
+        },
+})
